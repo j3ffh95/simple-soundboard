4ca1b9c163c7fd8201268b33397b242205cd3e84
j3ffh95 <j3ffh95@gmail.com>
2020-10-06 10:18:36 -0400

    Adding logic to each button

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 const sounds = ["applause", "boo", "gasp", "tada", "victory"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
+
+  document.body.appendChild(btn);
+});
+
+const stopSongs = () => {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+};
